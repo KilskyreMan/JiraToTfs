bc0a5d338dba7630a87298b184b87094a310bab7
Ian Montgomery <ian.montgomery@sage.com>
2016-03-11 16:50:39 +0000

    Added GNU license headers to source.

--- JiraToTfs/View/TicektsNotImportedView.cs
+++ JiraToTfs/View/TicektsNotImportedView.cs
@@ -1,4 +1,26 @@
-﻿using System;
+﻿#region License
+/*
+    This source makes up part of JiraToTfs, a utility for migrating Jira
+    tickets to Microsoft TFS.
+
+    Copyright(C) 2016  Ian Montgomery
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.If not, see<http://www.gnu.org/licenses/>.
+*/
+#endregion
+
+using System;
 using System.Collections.Generic;
 using System.Windows.Forms;
 using TicketImporter.Interface;
--- TicketImporter/JiraTypeMap.cs
+++ TicketImporter/JiraTypeMap.cs
@@ -1,7 +1,27 @@
-﻿using System;
+﻿#region License
+/*
+    This source makes up part of JiraToTfs, a utility for migrating Jira
+    tickets to Microsoft TFS.
+
+    Copyright(C) 2016  Ian Montgomery
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.If not, see<http://www.gnu.org/licenses/>.
+*/
+#endregion
+
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Principal;
 using TicketImporter.Interface;
 
 namespace TicketImporter
